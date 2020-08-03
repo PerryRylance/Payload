b614567418bdecbf4bdc0e2a0e877f7ca4d9e74c
Perry Rylance <perryrylance@googlemail.com>
2020-08-03 10:41:35 +0100

    CameraControls
    - Removed OrbitControls
    - Removed Payload.Interaction controls
    - Now using yomotsu/camera-controls
    - Added additional radius to ship spawn points to mitigate physics engine stalling

--- dist/js/main.js
+++ dist/js/main.js
@@ -1678,34 +1678,6 @@ Payload.Ship.prototype.initGraphics = function(options)
 	this.object3d = new THREE.Mesh(geometry, material);
 }
 // requires: core.js
-	
-Payload.WeaponSelect = function($element)
-{
-	this.$element = $element;
-	this.$element.html("");
-	
-	for(var name in Payload.Weapon)
-	{
-		var $option	= $("<option></option>");
-		var cost;
-		
-		if(Payload.Weapon[name].COST !== undefined)
-			cost	= Payload.Weapon[name].COST;
-		else
-		{
-			console.warn("Payload.Weapon." + name + " has no COST");
-			cost	= 0;
-		}
-		
-		var caption	= name.replace(/([A-Z])/g, " $1").trim(" ") + " (" + cost + ")";
-		
-		$option.attr("value", name);
-		$option.text(caption);
-		
-		$($element).append($option);
-	}
-}
-// requires: core.js
 		
 /**
  * Base class used for events (for non-HTMLElement objects)
@@ -1744,6 +1716,34 @@ Payload.Event.prototype.stopPropagation = function()
 	this._cancelled = true;
 }
 
+// requires: core.js
+	
+Payload.WeaponSelect = function($element)
+{
+	this.$element = $element;
+	this.$element.html("");
+	
+	for(var name in Payload.Weapon)
+	{
+		var $option	= $("<option></option>");
+		var cost;
+		
+		if(Payload.Weapon[name].COST !== undefined)
+			cost	= Payload.Weapon[name].COST;
+		else
+		{
+			console.warn("Payload.Weapon." + name + " has no COST");
+			cost	= 0;
+		}
+		
+		var caption	= name.replace(/([A-Z])/g, " $1").trim(" ") + " (" + cost + ")";
+		
+		$option.attr("value", name);
+		$option.text(caption);
+		
+		$($element).append($option);
+	}
+}
 // requires: entities/entity.js
 
 Payload.Weapon = function(world)
